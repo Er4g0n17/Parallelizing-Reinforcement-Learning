8f14951b1d953458bdbea2e4275bc53e0e416b36
Er4g0n17 <61225989+Er4g0n17@users.noreply.github.com>
2026-02-16 18:03:24 +0100

    Add files via upload

---
+++ dqn.py
@@ -0,0 +1,282 @@
+import gymnasium as gym
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import os
+import time
+import sys
+
+import pandas as ps
+from pathlib import Path
+
+env = gym.make("CartPole-v1")
+
+# if GPU is to be used
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+    
+episode_durations = []
+BATCH_SIZE = 128
+GAMMA = 0.99
+
+TAU = 0.005
+LR = 1e-4   
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+
+class Trainer:
+    def __init__(
+        self,
+        target_net: DQN,
+        policy_net: DQN,
+        optimizer: torch.optim.Optimizer,
+        memory: ReplayMemory,
+        gpu_id: int,
+        save_every: int,
+    ) -> None:
+        self.gpu_id = gpu_id
+        self.target_net = target_net.to(gpu_id)
+        self.policy_net = policy_net.to(gpu_id)
+        self.optimizer = optimizer
+        self.memory = memory
+        self.save_every = save_every
+        self.target_net = target_net
+        self.policy_net = policy_net
+        self.episode_durations = []
+        self.episode_times = []
+
+    def select_action(self, state):
+        sample = random.random()
+        eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+            math.exp(-1. * self.steps_done / EPS_DECAY)
+        self.steps_done += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                # t.max(1) will return the largest column value of each row.
+                # second column on max result is index of where max element was
+                # found, so we pick action with the larger expected reward.
+                return self.policy_net(state).max(1).indices.view(1, 1)
+        else:
+            return torch.tensor([[env.action_space.sample()]], device=self.gpu_id, dtype=torch.long)
+
+    def optimize_model(self):
+        if len(self.memory) < BATCH_SIZE:
+            return
+        transitions = self.memory.sample(BATCH_SIZE)
+        # This converts batch-array of Transitions
+        # to Transition of batch-arrays.
+        batch = Transition(*zip(*transitions))
+
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                            batch.next_state)), device=self.gpu_id, dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                                    if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1).values
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(BATCH_SIZE, device=self.gpu_id)
+        with torch.no_grad():
+            next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1).values
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+        # Compute Huber loss
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer.step()
+
+        return loss            
+
+    def _save_checkpoint(self, epoch, num_episodes):
+        ckp = self.policy_net.module.state_dict()
+        PATH = "checkpoint_" + str(num_episodes) + ".pt"
+        torch.save(ckp, PATH)
+        print(f"Epoch {epoch} | Training checkpoint saved at {PATH}")
+
+    def training(self, num_episodes):
+        self.steps_done = 0
+
+        losses = []
+        for i_episode in range(num_episodes):
+            # Initialize the environment and get it's state
+            state, info = env.reset()
+            state = torch.tensor(state, dtype=torch.float32, device=self.gpu_id).unsqueeze(0)
+            for t in count():
+                
+                action = self.select_action(state)
+                observation, reward, terminated, truncated, _ = env.step(action.item())
+                reward = torch.tensor([reward], device=self.gpu_id)
+                done = terminated or truncated
+
+                if terminated:
+                    next_state = None
+                else:
+                    next_state = torch.tensor(observation, dtype=torch.float32, device=self.gpu_id).unsqueeze(0)
+
+                # Store the transition in memory
+                self.memory.push(state, action, next_state, reward)
+
+                # Move to the next state
+                state = next_state
+
+                # Perform one step of the optimization (on the policy network)
+                loss = self.optimize_model()
+
+                # Soft update of the target network's weights
+                # θ′ ← τ θ + (1 −τ )θ′
+                target_net_state_dict = self.target_net.state_dict()
+                policy_net_state_dict = self.policy_net.state_dict()
+                for key in policy_net_state_dict:
+                    target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+                self.target_net.load_state_dict(target_net_state_dict)
+                
+                if i_episode % self.save_every == 0:
+                    self._save_checkpoint(i_episode, num_episodes)
+
+                if done:
+                    self.episode_durations.append(t + 1)
+                    losses.append(loss)
+                    break
+        return losses, self.gpu_id
+                
+    
+    def plot_durations(self, show_result=False):
+        plt.figure(1)
+        durations_t = torch.tensor(self.episode_durations, dtype=torch.float)
+        if show_result:
+            plt.title('Result')
+        else:
+            plt.clf()
+            plt.title('Training...')
+        plt.xlabel('Episode')
+        plt.ylabel('Duration')
+        plt.plot(durations_t.numpy())
+        # Take 100 episode averages and plot them too
+        if len(durations_t) >= 100:
+            means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+            means = torch.cat((torch.zeros(99), means))
+            plt.plot(means.numpy())
+
+
+
+    
+def load_train_objs():
+    # Get number of actions from gym action space
+    n_actions = env.action_space.n
+    # Get the number of state observations
+    state, info = env.reset()
+    n_observations = len(state)
+
+    policy_net = DQN(n_observations, n_actions)
+    target_net = DQN(n_observations, n_actions)
+    target_net.load_state_dict(policy_net.state_dict())
+
+    optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+    memory = ReplayMemory(10000)
+
+    return policy_net, target_net, optimizer, memory
+
+
+
+def write_csv(losses, gpu_id, total_epochs):
+    path = Path('run_seq_' + str(gpu_id) + "_" + str(total_epochs) + '.csv')
+    for loss in losses:
+        inp = {"losses": loss}
+        df = ps.DataFrame([inp])
+        include_header = not path.exists()
+        df.to_csv(path, mode="a", header= include_header)
+
+
+
+def main(rank: int, world_size: int, save_every: int, total_epochs: int, batch_size: int):
+    policy_net, target_net, optimizer, memory = load_train_objs()
+    trainer = Trainer(target_net, policy_net, optimizer, memory, rank, save_every)
+    losses, gpu_id = trainer.training(total_epochs)
+
+    write_csv(losses, gpu_id, total_epochs)
+
+    print("FINISHED")
+   
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='simple distributed training job')
+    parser.add_argument('total_epochs', type=int, help='Total epochs to train the model')
+    parser.add_argument('save_every', type=int, help='How often to save a snapshot')
+    parser.add_argument('--batch_size', default=128, type=int, help='Input batch size on each device (default: 32)')
+    args = parser.parse_args()
+    
+    world_size = torch.cuda.device_count()
+    print(world_size)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    flag = time.time()
+    main(device, world_size, args.save_every, args.total_epochs, args.batch_size)
+    print(time.time() - flag)
+    
+
+
+
+
+
+
